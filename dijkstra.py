76bcd3fe03a1137f9d12ad1960f435b2886832ba
iqDF <daniel.thekurniadi@gmail.com>
2020-02-04 00:46:38 +0800

    - rename heapQueoe to openSet - return openSet, closedSet

--- dijkstra.py
+++ dijkstra.py
@@ -1,13 +1,22 @@
 """
 """
+
 from heapq import heapify, heappop, heappush
-# from models import GridBoard, Tile
+
 
 class Dijkstra(object):
     """ Path finder algorithm handler
     """
 
     def __init__(self, grid, startCoor, endCoor, adjancentDist=1):
+        """ Initialise Dijkstra solver for new problem
+        :type grid: GridBoard
+        :type startCoor: int
+        :type encCoor: int
+        :type adjacentDist: int
+        """
+        assert len(startCoor) == 2
+        assert len(endCoor) == 2
         xStart, yStart = startCoor
         xEnd, yEnd = endCoor
 
@@ -15,26 +24,40 @@ class Dijkstra(object):
         self.grid = grid
         self.startTile = self.grid.get(xStart, yStart)
         self.endTile = self.grid.get(xEnd, yEnd)
+
+        # checking if valid start and end
+        if not self.startTile:
+            raise ValueError("Start position is out of grid bound or invalid: " + str(startCoor))
+
+        if not self.endTile:
+            raise ValueError("End goal position is out of grid bound or invalid: " + str(endCoor))
+
+        # setup state and track record
+        self.openSet = heapify([(0, self.startTile)])
+        self.closedSet = set()
+        self.trackRecords = {}
         self.reachedEnd = False
 
-        self.heapQueue = heapify([(0, self.startTile)])
-        self.trackRecords = {}
-
     def step(self):
-        if not self.heapQueue: return None
+        """ Dijkstra per step implementation
+        :rtype (List[Cell], Set{Cell}): data structures representing path finding state
+        """
+        if not self.openSet or self.reachedEnd == True:
+            return self.openSet, self.closedSet
 
-        dist, currentTile = heappop(self.heapQueue)  # comment: heapq doesn't support key min, but this is redudant
-        currentTile.closed = True
+        dist, currentCell = heappop(self.openSet)  # comment: heapq doesn't support key min, but this is redudant
+        self.closedSet.add(currentCell)
 
-        if currentTile == self.endTile:
+        if currentCell == self.endTile:
             self.reachedEnd = True
-            return currentTile
+            return currentCell
 
-        for neighbour in self.grid.getNeighbours(currentTile):
-            if neighbour.closed == True: continue
+        for neighbour in self.grid.getNeighbours(currentCell):
+            if currentCell in self.closedSet: continue
 
             alt = dist + self.adjancentDist
             if alt < neighbour.distance:
                 neighbour.distance = alt
-                neighbour.opened = True
-        return None
+                heappush(self.openSet, (alt, neighbour))
+
+        return self.openSet, self.closedSet  # state of the path-finding
